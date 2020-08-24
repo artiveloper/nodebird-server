b64fb8fece5134a3948e347a140a368df331ff09
ravi <artiveloper@gmail.com>
2020-08-24 14:03:46 +0900

    fix: development 환경일 경우에만 sequelize sync 시 테이블을 생성하도록 수정한다

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ import express from 'express'
 import routerRegister from './routes/register'
 import db from './models'
 
-db.sequelize.sync()
+// database configuration
+const databaseOptions = {
+    force: process.env.NODE_ENV === 'development' ? true : false
+}
+db.sequelize.sync(databaseOptions)
     .then(() => {
         console.log('db 연결 성공')
     })
     .catch(console.log)
 
+// express configuration
 const app = express()
 
 routerRegister(app)
